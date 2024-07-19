c41dcaac0624033d81ea9aaa7bd5b974eb2eb1e0
jimmy <jimmyjiang70@gmail.com>
2024-07-18 21:46:29 -0400

    add jsoup to maven and testing fetching problems from lc

--- src/main/java/ashoyo1/leetcodeproblems.java
+++ src/main/java/ashoyo1/leetcodeproblems.java
@@ -1,5 +1,49 @@
 package ashoyo1;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class leetcodeproblems {
-    
+
+    private static final int  max = 100; 
+
+    public static void main(String[] args) {
+
+        List<String[]> problems = new ArrayList<>();
+        int count = 0;
+        int page = 1;  // track pages and use to make to next pg
+
+        while (count < max) {
+
+            Document doc = Jsoup.connect("https://leetcode.com/problemset/all/?page=" + page).get();
+            Elements rows = doc.select(".reactable-data tr");
+
+            for (Element row : rows) {
+                if (count >= max)
+                 break;
+
+                Elements cols = row.select("td");
+                
+                if (cols.size() > 0) {
+                    String title = cols.get(2).text();
+                    String difficulty = cols.get(5).text();
+                    String url = "https://leetcode.com" + cols.get(2).select("a").attr("href");
+
+                    problems.add(new String[]{title, difficulty, url});
+                    count++;
+                }
+            }
+            page++; // Move to the next page
+        }
+
+  
+        for (String[] problem : problems) {
+            System.out.println("Title: " + problem[0] + ", Difficulty: " + problem[1] + ", URL: " + problem[2]);
+        }
+    }
 }

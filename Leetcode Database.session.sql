dda14284cc45de74cf26422a7c7e178ac1524546
jimmy <jimmyjiang70@gmail.com>
2024-07-18 22:03:34 -0400

    created new table was messed up the order

--- Leetcode Database.session.sql
+++ Leetcode Database.session.sql
@@ -1,7 +1,9 @@
 USE leetcode_leaderboard;
 
 CREATE TABLE LeetCodeProblems (
+    problem_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
     difficulty ENUM('Easy', 'Medium', 'Hard'),
     url VARCHAR(255)
 );
+
